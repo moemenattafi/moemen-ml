a8b26390b606df5a12a43979898bb10952b70101
moemen <attafimoemen21@gmail.com>
2023-11-30 10:16:40 +0100

    just svm is working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request,render_template, jsonify
-import tensorflow as tf
+#import tensorflow as tf
 import joblib
 import librosa
 import numpy as np
@@ -9,55 +9,55 @@ app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load("svm_model.joblib")
-model_vgg = tf.keras.models.load_model('best_model.h5')
+#model_vgg = tf.keras.models.load_model('best_model.h5')
 
 # Function to extract features from an audio file
-def extract_mel_spectrogram(audio_data):
-    try:
-        audio, sr = librosa.load(audio_data, res_type='kaiser_fast')
-        mel_spectrogram = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=128)
-        mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
-        return mel_spectrogram
-    except Exception as e:
-        raise RuntimeError(f"Error extracting mel spectrogram from audio data: {e}")
+# def extract_mel_spectrogram(audio_data):
+#     try:
+#         audio, sr = librosa.load(audio_data, res_type='kaiser_fast')
+#         mel_spectrogram = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=128)
+#         mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
+#         return mel_spectrogram
+#     except Exception as e:
+#         raise RuntimeError(f"Error extracting mel spectrogram from audio data: {e}")
 
 # Function to preprocess mel spectrogram for VGG16 model
-def preprocess_mel_spectrogram(mel_spectrogram):
-    mel_spectrogram = np.expand_dims(mel_spectrogram, axis=-1)
-    mel_spectrogram = np.expand_dims(mel_spectrogram, axis=0)
-    mel_spectrogram = mel_spectrogram / 255.0  # Normalize to [0, 1]
-    return mel_spectrogram
+# def preprocess_mel_spectrogram(mel_spectrogram):
+#     mel_spectrogram = np.expand_dims(mel_spectrogram, axis=-1)
+#     mel_spectrogram = np.expand_dims(mel_spectrogram, axis=0)
+#     mel_spectrogram = mel_spectrogram / 255.0  # Normalize to [0, 1]
+#     return mel_spectrogram
 
-@app.route('/predict_genre', methods=['POST'])
-def predict_genre():
-    try:
-        # Get the audio data from the request
-        audio_data = request.files['file']
+# @app.route('/predict_genre', methods=['POST'])
+# def predict_genre():
+#     try:
+#         # Get the audio data from the request
+#         audio_data = request.files['file']
 
-        # Save the file temporarily
-        file_path = "vgg16.wav"
-        audio_data.save(file_path)
+#         # Save the file temporarily
+#         file_path = "vgg16.wav"
+#         audio_data.save(file_path)
 
-        # Extract mel spectrogram from the audio data
-        mel_spectrogram = extract_mel_spectrogram(file_path)
+#         # Extract mel spectrogram from the audio data
+#         mel_spectrogram = extract_mel_spectrogram(file_path)
 
-        # Preprocess mel spectrogram for VGG16 model
-        input_data = preprocess_mel_spectrogram(mel_spectrogram)
-        print("Input shape:", input_data.shape)
-        print("Input data type:", input_data.dtype)
-        # Make a prediction using the VGG16 model
-        prediction = model_vgg.predict(input_data)
-        print("🚀 ~ file: app.py:49 ~ prediction:", prediction)
+#         # Preprocess mel spectrogram for VGG16 model
+#         input_data = preprocess_mel_spectrogram(mel_spectrogram)
+#         print("Input shape:", input_data.shape)
+#         print("Input data type:", input_data.dtype)
+#         # Make a prediction using the VGG16 model
+#         prediction = model_vgg.predict(input_data)
+#         print("🚀 ~ file: app.py:49 ~ prediction:", prediction)
 
-        # Get the predicted genre
-        predicted_genre = get_genre_from_label(np.argmax(prediction))
-        print("🚀 ~ file: app.py:56 ~ predicted_genre:", predicted_genre)
+#         # Get the predicted genre
+#         predicted_genre = get_genre_from_label(np.argmax(prediction))
+#         print("🚀 ~ file: app.py:56 ~ predicted_genre:", predicted_genre)
 
-        # Return the result as JSON
-        result = {'prediction': predicted_genre}
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+#         # Return the result as JSON
+#         result = {'prediction': predicted_genre}
+#         return jsonify(result)
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
 
 # Function to extract features from an audio file
 def extract_features(file_path):
--- vgg16_model.py
+++ vgg16_model.py
@@ -1,68 +1,69 @@
-import tensorflow as tf
-from tensorflow.keras import layers, models
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras.optimizers import Adam
+# import tensorflow as tf
+# from tensorflow.keras import layers, models
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras.applications import VGG16
+# from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+# from tensorflow.keras.optimizers import Adam
 
-# Load pre-trained VGG16 model
-model = VGG16(include_top=False, input_shape=(300, 300, 3))
+# # Load pre-trained VGG16 model
+# model = VGG16(include_top=False, input_shape=(300, 300, 3))
 
-# Freeze the layers of the pre-trained model
-for layer in model.layers:
-    layer.trainable = False
+# # Freeze the layers of the pre-trained model
+# for layer in model.layers:
+#     layer.trainable = False
 
-# Define data augmentation parameters
-data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
-    rescale=1./255.,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    validation_split=0.2  # 20% of data for validation
-)
+# # Define data augmentation parameters
+# data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+#     rescale=1./255.,
+#     rotation_range=20,
+#     width_shift_range=0.2,
+#     height_shift_range=0.2,
+#     shear_range=0.2,
+#     zoom_range=0.2,
+#     horizontal_flip=True,
+#     validation_split=0.2  # 20% of data for validation
+# )
 
-# Load training and validation data
-path = './data/images_original'
-train_datagen = data_gen.flow_from_directory(
-    path, target_size=(300, 300), batch_size=32, class_mode='categorical', subset='training'
-)
-val_datagen = data_gen.flow_from_directory(
-    path, target_size=(300, 300), batch_size=32, class_mode='categorical', subset='validation'
-)
+# # Load training and validation data
+# path = './data/images_original'
+# train_datagen = data_gen.flow_from_directory(
+#     path, target_size=(300, 300), batch_size=32, class_mode='categorical', subset='training'
+# )
+# val_datagen = data_gen.flow_from_directory(
+#     path, target_size=(300, 300), batch_size=32, class_mode='categorical', subset='validation'
+# )
 
-# Define model architecture
-output = model.layers[-1].output
-model_final = tf.keras.layers.Flatten()(output)
-model_final = tf.keras.layers.Dense(512, activation='relu')(model_final)
-model_final = tf.keras.layers.Dense(64, activation='relu')(model_final)
-model_final = tf.keras.layers.Dense(10, activation='softmax')(model_final)
-model = tf.keras.models.Model(model.input, model_final)
+# # Define model architecture
+# output = model.layers[-1].output
+# model_final = tf.keras.layers.Flatten()(output)
+# model_final = tf.keras.layers.Dense(512, activation='relu')(model_final)
+# model_final = tf.keras.layers.Dense(64, activation='relu')(model_final)
+# model_final = tf.keras.layers.Dense(10, activation='softmax')(model_final)
+# model = tf.keras.models.Model(model.input, model_final)
 
-# Compile the model with Adam optimizer and categorical crossentropy loss
-model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+# # Compile the model with Adam optimizer and categorical crossentropy loss
+# model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# ModelCheckpoint callback to save the best weights
-checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+# # ModelCheckpoint callback to save the best weights
+# checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
 
-# LearningRateScheduler callback for dynamic learning rate adjustment
-def lr_schedule(epoch):
-    if epoch < 5:
-        return 0.001
-    elif 5 <= epoch < 8:
-        return 0.0005
-    else:
-        return 0.0001
+# # LearningRateScheduler callback for dynamic learning rate adjustment
+# def lr_schedule(epoch):
+#     if epoch < 5:
+#         return 0.001
+#     elif 5 <= epoch < 8:
+#         return 0.0005
+#     else:
+#         return 0.0001
 
-lr_scheduler = LearningRateScheduler(lr_schedule)
+# lr_scheduler = LearningRateScheduler(lr_schedule)
 
-# Train the model with training and validation data
-model.fit(
-    train_datagen,
-    epochs=10,
-    validation_data=val_datagen,
-    callbacks=[checkpoint, lr_scheduler],
-    verbose=2
-)
+# # Train the model with training and validation data
+# model.fit(
+#     train_datagen,
+#     epochs=10,
+#     validation_data=val_datagen,
+#     callbacks=[checkpoint, lr_scheduler],
+#     verbose=2
+# )
+
